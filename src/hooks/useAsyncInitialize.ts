f7488a1640c9e1b60b02eef97a7742da6c43e866
liuketao <89596593@qq.com>
2024-09-13 10:43:26 +0800

    Initial commit

---
+++ src/hooks/useAsyncInitialize.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+export function useAsyncInitialize<T>(func: () => Promise<T>, deps: readonly unknown[] = []) {
+  const [state, setState] = useState<T | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      setState(await func());
+    })();
+  }, deps);
+
+  return state;
+}
